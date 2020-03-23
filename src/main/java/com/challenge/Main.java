264994a48f30c96b10204546fe8e43023f938212
k3y5tr0k3 <bryandonnelly7@gmail.com>
2020-03-23 17:21:20 +0000

    recreated project and dockerized

--- src/main/java/com/challenge/Main.java
+++ src/main/java/com/challenge/Main.java
@@ -1,10 +1,45 @@
 package com.challenge;
 
-class Main {
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 
-     public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
 
-        System.out.println("Hello world!");
+/**
+ * Main class.
+ *
+ */
+public class Main {
+    // Base URI internal to container
+    public static final String BASE_URI = "http://0.0.0.0:8080/myapp/";
 
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in com.challenge package
+        final ResourceConfig rc = new ResourceConfig().packages("com.challenge");
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.in.read();
+        server.stop();
     }
 }
+
